2259f0316c6ad7487b7cd2ba694facd73fb92065
JonnyA <andrushaes@gmail.com>
2020-09-21 09:40:06 +0300

    add

---
+++ build/precache-manifest.488f51f148bdb4847fb1e16695b0cc30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc694aac18e4085617a9354429d4bcaa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1bb45c09f3b5780a367b",
+    "url": "/static/css/main.ad0f0d0e.chunk.css"
+  },
+  {
+    "revision": "e952a599ae6296fa2c24",
+    "url": "/static/js/2.76b0790e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.76b0790e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bb45c09f3b5780a367b",
+    "url": "/static/js/main.09fc0455.chunk.js"
+  },
+  {
+    "revision": "bbf8e8a34b364a92fb76",
+    "url": "/static/js/runtime-main.01d5ece0.js"
+  }
+]);
