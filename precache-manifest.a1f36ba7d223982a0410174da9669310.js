ce034df19310aca3f4609267a614b419360b9e9a
andrusha52 <andrushaes@gmail.com>
2020-09-21 09:47:10 +0300

    Updates

---
+++ precache-manifest.a1f36ba7d223982a0410174da9669310.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abb12e631a4f6da8716429b30a98a5c1",
+    "url": "/YliaDR/index.html"
+  },
+  {
+    "revision": "1bb45c09f3b5780a367b",
+    "url": "/YliaDR/static/css/main.ad0f0d0e.chunk.css"
+  },
+  {
+    "revision": "e952a599ae6296fa2c24",
+    "url": "/YliaDR/static/js/2.76b0790e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/YliaDR/static/js/2.76b0790e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bb45c09f3b5780a367b",
+    "url": "/YliaDR/static/js/main.09fc0455.chunk.js"
+  },
+  {
+    "revision": "d75e7a43570c7e2fae73",
+    "url": "/YliaDR/static/js/runtime-main.11bd9377.js"
+  }
+]);
